0fdfac31d8f69b14b48589e65cb4fd4c18eb53bb
eden_sobol <edenwesleysobol@gmail.com>
2023-07-23 19:14:55 -0400

    first commit

---
+++ src/styles/main.tsx
@@ -0,0 +1,14 @@
+import css from "styled-jsx/css";
+
+const color: string = "green";
+
+export default css`
+	main {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		background: ${color};
+	}
+`;
